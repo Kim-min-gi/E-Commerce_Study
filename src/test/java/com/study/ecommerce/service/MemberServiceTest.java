a22b77ac12a945e7e652f5aa7132871c1aa863e1
DANGHYEON <rlaalsrl3023@naver.com>
2024-05-30 16:20:01 +0900

    MemberServiceTest 수정

--- src/test/java/com/study/ecommerce/service/MemberServiceTest.java
+++ src/test/java/com/study/ecommerce/service/MemberServiceTest.java
@@ -2,11 +2,15 @@ package com.study.ecommerce.service;
 
 import com.study.ecommerce.domain.Member;
 import com.study.ecommerce.repository.MemberRepository;
+import com.study.ecommerce.response.MemberResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,9 +32,17 @@ class MemberServiceTest {
 
         memberRepository.save(member);
 
-        int count = memberRepository.findAll().size();
+        int count = memberRepository.findAll().stream().map(MemberResponse::new).toList().size();
+
+        List<MemberResponse> list = memberRepository.findAll().stream().map(MemberResponse::new).toList();
+
+
 
         Assertions.assertEquals(1,count);
+        Assertions.assertEquals("testing@gmail.com",list.get(0).getEmail());
+        Assertions.assertEquals("name",list.get(0).getName());
+        Assertions.assertEquals(1L,list.get(0).getId());
+
 
     }
 }
