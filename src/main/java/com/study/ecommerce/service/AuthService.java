290fc2a41ec9006588659178c3429b77c8977b8b
DANGHYEON <rlaalsrl3023@naver.com>
2024-06-09 14:47:18 +0900

    signup return 값 추가

--- src/main/java/com/study/ecommerce/service/AuthService.java
+++ src/main/java/com/study/ecommerce/service/AuthService.java
@@ -5,22 +5,39 @@ import com.study.ecommerce.repository.MemberRepository;
 import com.study.ecommerce.request.MemberSignUp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AuthService {
 
     private final MemberRepository memberRepository;
 
 
-    public void signup(MemberSignUp memberSignUp){
+    public long signup(MemberSignUp memberSignUp){
 
         //중복체크
 
 
         //security passwordEncoder 설정
 
-        memberRepository.save(memberSignUp.toEntity());
+        Member member = memberRepository.save(memberSignUp.toEntity());
+
+        return member.getId();
+
+    }
+
+
+
+    public void resign(long id){
+
+        Optional<Member> member = memberRepository.findById(id);
+
+        member.ifPresent(memberRepository::delete);
+
     }
 
 
