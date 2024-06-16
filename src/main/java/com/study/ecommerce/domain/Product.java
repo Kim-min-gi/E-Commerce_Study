c0849b3e9b3ec9b1d0d6e7f81879759708499f3a
DANGHYEON <rlaalsrl3023@naver.com>
2024-06-16 21:33:45 +0900

    product domain 생성

---
+++ src/main/java/com/study/ecommerce/domain/Product.java
@@ -0,0 +1,41 @@
+package com.study.ecommerce.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int bill;
+
+    @Column(nullable = false)
+    private int count;
+
+
+
+    @Builder
+    public Product(String name, int bill, int count) {
+        this.name = name;
+        this.bill = bill;
+        this.count = count;
+    }
+
+
+
+}
