f7d7d4908621d6d5339798dafb6e591aa12e7d21
DANGHYEON <rlaalsrl3023@naver.com>
2024-06-19 18:31:30 +0900

    JWTUtil 생성

---
+++ src/main/java/com/study/ecommerce/config/JwtUtil.java
@@ -0,0 +1,62 @@
+package com.study.ecommerce.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.print.DocFlavor;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+
+    private final SecretKey secretKey;
+
+
+    public JwtUtil(@Value("${spring.jwtSecretKey}")String secretKey){
+        this.secretKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    //검증을 진행
+    public String getEmail(String token){
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("email", String.class);
+    }
+
+    public String getRole(String token){
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    //토큰 만료 검증 진행
+    public Boolean isExpired(String token){
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+
+
+    public String createToken(String email, Long expiredMs){
+
+        return Jwts.builder()
+                .claim("email", email)
+                //.claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+
+
+
+
+
+
+
+}
