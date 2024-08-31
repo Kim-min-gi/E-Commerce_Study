98f14c2d85c8c85502ebf9a91e578e35629bcfa6
Min <74136649+DANGHYEON@users.noreply.github.com>
2024-08-31 11:11:25 +0900

    CartService path, remove Test 및 Controller Test 완료

--- src/test/java/com/study/ecommerce/controller/CartControllerTest.java
+++ src/test/java/com/study/ecommerce/controller/CartControllerTest.java
@@ -1,5 +1,7 @@
 package com.study.ecommerce.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.ecommerce.config.CustomMockMember;
 import com.study.ecommerce.domain.Cart;
 import com.study.ecommerce.domain.Member;
@@ -9,6 +11,7 @@ import com.study.ecommerce.repository.CartRepository;
 import com.study.ecommerce.repository.MemberRepository;
 import com.study.ecommerce.repository.ProductCategoryRepository;
 import com.study.ecommerce.repository.ProductRepository;
+import com.study.ecommerce.request.CartRequest;
 import com.study.ecommerce.service.CartService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +19,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -35,6 +39,9 @@ class CartControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     private CartRepository cartRepository;
@@ -117,14 +124,193 @@ class CartControllerTest {
     }
 
     @Test
-    void addCartProduct() {
+    @DisplayName("카트 상품 추가")
+    @CustomMockMember(role = "ROLE_USER")
+    void addCartProduct() throws Exception{
+        //given
+        Member member = Member.builder()
+                .email("Testing@naver.com")
+                .name("Test")
+                .role("ROLE_USER")
+                .password("ASDA515184424")
+                .build();
+
+        memberRepository.save(member);
+
+        ProductCategory productCategory = ProductCategory.builder()
+                .name("카테고리1")
+                .build();
+
+        productCategoryRepository.save(productCategory);
+
+        Product product = Product.builder()
+                .name("물품1")
+                .price(1000)
+                .quantity(10)
+                .build();
+
+        Product product2 = Product.builder()
+                .name("물품2")
+                .price(51)
+                .quantity(100)
+                .build();
+
+        product.setCategory(productCategory);
+        product2.setCategory(productCategory);
+
+        productRepository.save(product);
+        productRepository.save(product2);
+
+        CartRequest cartRequest = CartRequest.builder()
+                .productId(product.getId())
+                .quantity(3)
+                .build();
+
+        CartRequest cartRequest2 = CartRequest.builder()
+                .productId(product2.getId())
+                .quantity(5)
+                .build();
+
+
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("Testing@naver.com", null, authorities)
+        );
+
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/cart")
+                        .content(objectMapper.writeValueAsString(cartRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
-    void modifyCartProduct() {
+    @DisplayName("카트 상품 수량 수정")
+    @CustomMockMember(role = "ROLE_USER")
+    void modifyCartProduct() throws Exception {
+        //given
+        Member member = Member.builder()
+                .email("Testing@naver.com")
+                .name("Test")
+                .role("ROLE_USER")
+                .password("ASDA515184424")
+                .build();
+
+        memberRepository.save(member);
+
+        ProductCategory productCategory = ProductCategory.builder()
+                .name("카테고리1")
+                .build();
+
+        productCategoryRepository.save(productCategory);
+
+        Product product = Product.builder()
+                .name("물품1")
+                .price(1000)
+                .quantity(10)
+                .build();
+
+        Product product2 = Product.builder()
+                .name("물품2")
+                .price(51)
+                .quantity(100)
+                .build();
+
+        product.setCategory(productCategory);
+        product2.setCategory(productCategory);
+
+        productRepository.save(product);
+        productRepository.save(product2);
+
+        CartRequest cartRequest = CartRequest.builder()
+                .productId(product.getId())
+                .quantity(3)
+                .build();
+
+        Cart cart = Cart.createCart(member,product,cartRequest.getQuantity());
+
+        cartRepository.save(cart);
+
+        CartRequest cartRequest2 = CartRequest.builder()
+                .productId(product.getId())
+                .quantity(5)
+                .build();
+
+
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("Testing@naver.com", null, authorities)
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/cart")
+                    .content(objectMapper.writeValueAsString(cartRequest2))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNoContent())
+                .andDo(MockMvcResultHandlers.print());
+
     }
 
     @Test
-    void removeCartProduct() {
+    @DisplayName("카트 상품 삭제")
+    @CustomMockMember(role = "ROLE_USER")
+    void removeCartProduct() throws Exception{
+        //given
+        Member member = Member.builder()
+                .email("Testing@naver.com")
+                .name("Test")
+                .role("ROLE_USER")
+                .password("ASDA515184424")
+                .build();
+
+        memberRepository.save(member);
+
+        ProductCategory productCategory = ProductCategory.builder()
+                .name("카테고리1")
+                .build();
+
+        productCategoryRepository.save(productCategory);
+
+        Product product = Product.builder()
+                .name("물품1")
+                .price(1000)
+                .quantity(10)
+                .build();
+
+        Product product2 = Product.builder()
+                .name("물품2")
+                .price(51)
+                .quantity(100)
+                .build();
+
+        product.setCategory(productCategory);
+        product2.setCategory(productCategory);
+
+        productRepository.save(product);
+        productRepository.save(product2);
+
+        CartRequest cartRequest = CartRequest.builder()
+                .productId(product.getId())
+                .quantity(3)
+                .build();
+
+        Cart cart = Cart.createCart(member,product,cartRequest.getQuantity());
+
+        cartRepository.save(cart);
+
+
+
+
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("Testing@naver.com", null, authorities)
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/cart")
+                        .content(objectMapper.writeValueAsString(cartRequest))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNoContent())
+                .andDo(MockMvcResultHandlers.print());
+
     }
 }
--- src/test/java/com/study/ecommerce/service/CartServiceTest.java
+++ src/test/java/com/study/ecommerce/service/CartServiceTest.java
@@ -8,11 +8,10 @@ import com.study.ecommerce.repository.CartRepository;
 import com.study.ecommerce.repository.MemberRepository;
 import com.study.ecommerce.repository.ProductCategoryRepository;
 import com.study.ecommerce.repository.ProductRepository;
+import com.study.ecommerce.request.CartRequest;
+import com.study.ecommerce.request.CategoryRequest;
 import com.study.ecommerce.response.CartListResponse;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -41,36 +40,252 @@ class CartServiceTest {
     private ProductRepository productRepository;
 
 
-    @BeforeEach
+    @AfterEach
     void clean(){
-        productCategoryRepository.deleteAll();
         cartRepository.deleteAll();
-        productRepository.deleteAll();
         memberRepository.deleteAll();
+        productCategoryRepository.deleteAll();
+        productRepository.deleteAll();
     }
 
 
     @Test
-    @DisplayName("장바구니에 상품 추가")
+    @DisplayName("장바구니에 상품 한개 추가")
     public void test1(){
+        //given
+        Member member = Member.builder()
+                .email("Testing@naver.com")
+                .name("Test")
+                .role("ROLE_USER")
+                .password("ASDA515184424")
+                .build();
 
+        memberRepository.save(member);
+
+        ProductCategory productCategory = ProductCategory.builder()
+                .name("카테고리1")
+                .build();
+
+        productCategoryRepository.save(productCategory);
+
+        Product product = Product.builder()
+                .name("물품1")
+                .price(1000)
+                .quantity(10)
+                .build();
+
+
+        product.setCategory(productCategory);
+
+        productRepository.save(product);
+
+        CartRequest cartRequest = CartRequest.builder()
+                .productId(product.getId())
+                .quantity(3)
+                .build();
+
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("Testing@naver.com", null, authorities)
+        );
+
+
+
+
+        //when
+        cartService.addCartProduct(cartRequest);
+
+
+        //then
+        List<CartListResponse> listCart = cartService.getCartList();
+
+        Assertions.assertEquals(listCart.size(),1);
+        Assertions.assertEquals(listCart.get(0).getQuantity(),3);
+        Assertions.assertEquals(listCart.get(0).getProductName(),"물품1");
+
+    }
+
+    @Test
+    @DisplayName("장바구니에 상품 두개 추가")
+    public void test2(){
+        //given
+        Member member = Member.builder()
+                .email("Testing@naver.com")
+                .name("Test")
+                .role("ROLE_USER")
+                .password("ASDA515184424")
+                .build();
+
+        memberRepository.save(member);
+
+        ProductCategory productCategory = ProductCategory.builder()
+                .name("카테고리1")
+                .build();
+
+        productCategoryRepository.save(productCategory);
+
+        Product product = Product.builder()
+                .name("물품1")
+                .price(1000)
+                .quantity(10)
+                .build();
+        Product product2 = Product.builder()
+                .name("물품2")
+                .price(10000)
+                .quantity(40)
+                .build();
+
+
+        product.setCategory(productCategory);
+        product2.setCategory(productCategory);
+
+        productRepository.save(product);
+        productRepository.save(product2);
+
+        CartRequest cartRequest = CartRequest.builder()
+                .productId(product.getId())
+                .quantity(3)
+                .build();
+        CartRequest cartRequest2 = CartRequest.builder()
+                .productId(product2.getId())
+                .quantity(1)
+                .build();
+
+
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("Testing@naver.com", null, authorities)
+        );
+
+
+        //when
+        cartService.addCartProduct(cartRequest);
+        cartService.addCartProduct(cartRequest2);
+
+        //then
+        List<CartListResponse> listCart = cartService.getCartList();
+
+        Assertions.assertEquals(listCart.size(),2);
+        Assertions.assertEquals(listCart.get(0).getQuantity(),3);
+        Assertions.assertEquals(listCart.get(0).getProductName(),"물품1");
+        Assertions.assertEquals(listCart.get(1).getQuantity(),1);
+        Assertions.assertEquals(listCart.get(1).getProductName(),"물품2");
     }
 
     @Test
     @DisplayName("장바구니에 상품 삭제")
-    public void test2(){
+    public void test3(){
+        //given
+        Member member = Member.builder()
+                .email("Testing@naver.com")
+                .name("Test")
+                .role("ROLE_USER")
+                .password("ASDA515184424")
+                .build();
+
+        memberRepository.save(member);
+
+        ProductCategory productCategory = ProductCategory.builder()
+                .name("카테고리1")
+                .build();
+
+        productCategoryRepository.save(productCategory);
+
+        Product product = Product.builder()
+                .name("물품1")
+                .price(1000)
+                .quantity(10)
+                .build();
+
+
+        product.setCategory(productCategory);
+
+        productRepository.save(product);
+
+        CartRequest cartRequest = CartRequest.builder()
+                .productId(product.getId())
+                .quantity(3)
+                .build();
+
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("Testing@naver.com", null, authorities)
+        );
+
+        cartService.addCartProduct(cartRequest);
+
+        //when
+        cartService.removeCartProduct(cartRequest);
+
+
+        //then
+        List<CartListResponse> listCart = cartService.getCartList();
+
+        Assertions.assertEquals(listCart.size(),0);
 
     }
 
     @Test
     @DisplayName("장바구니에 상품 수량 변경")
-    public void test3(){
+    public void test4(){
+        //given
+        Member member = Member.builder()
+                .email("Testing@naver.com")
+                .name("Test")
+                .role("ROLE_USER")
+                .password("ASDA515184424")
+                .build();
 
+        memberRepository.save(member);
+
+        ProductCategory productCategory = ProductCategory.builder()
+                .name("카테고리1")
+                .build();
+
+        productCategoryRepository.save(productCategory);
+
+        Product product = Product.builder()
+                .name("물품1")
+                .price(1000)
+                .quantity(10)
+                .build();
+
+
+        product.setCategory(productCategory);
+
+        productRepository.save(product);
+
+        CartRequest cartRequest = CartRequest.builder()
+                .productId(product.getId())
+                .quantity(3)
+                .build();
+
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("Testing@naver.com", null, authorities)
+        );
+
+        cartService.addCartProduct(cartRequest);
+
+        //when
+        CartRequest cartRequest2 = CartRequest.builder()
+                .productId(product.getId())
+                .quantity(1)
+                .build();
+
+        cartService.modifyCartProduct(cartRequest2);
+
+        //then
+        List<CartListResponse> listCart = cartService.getCartList();
+
+        Assertions.assertEquals(listCart.size(),1);
+        Assertions.assertEquals(listCart.get(0).getProductName(),"물품1");
+        Assertions.assertEquals(listCart.get(0).getQuantity(),1);
     }
 
     @Test
     @DisplayName("장바구니에 상품리스트 출력")
-    public void test4(){
+    public void test5(){
         //given
         Member member = Member.builder()
                 .email("Testing@naver.com")
