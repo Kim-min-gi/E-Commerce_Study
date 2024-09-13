5c7ef3e5c3d9a3fdb23665b84ba21687dcb924b9
DANGHYEON <rlaalsrl3023@naver.com>
2024-09-13 09:21:41 +0900

    OrderStatus ORDER_COMPLETE 추가

--- src/main/java/com/study/ecommerce/domain/type/OrderStatus.java
+++ src/main/java/com/study/ecommerce/domain/type/OrderStatus.java
@@ -3,6 +3,7 @@ package com.study.ecommerce.domain.type;
 public enum OrderStatus {
 
     CANCELED,
+    ORDER_COMPLETE, //주문완료
     PROCESSING, //출고중
     SHIPPED, //출고완료
     IN_DELIVERY, // 배송중
--- src/main/java/com/study/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/study/ecommerce/repository/OrderRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
     Page<Order> findByMember(Member member,Pageable pageable);
+
+    Page<Order> findByMember(Member member, Pageable pageable, LocalDate startDate, LocalDate endDate);
 
 }
