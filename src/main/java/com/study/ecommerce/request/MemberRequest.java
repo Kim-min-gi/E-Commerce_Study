08517c9f70808ec89e7d16ced2426bd33d891829
DANGHYEON <rlaalsrl3023@naver.com>
2025-01-25 22:27:30 +0900

    MemberRequest DTO 생성

---
+++ src/main/java/com/study/ecommerce/request/MemberRequest.java
@@ -0,0 +1,17 @@
+package com.study.ecommerce.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberRequest {
+
+    private Long id;
+
+
+}
