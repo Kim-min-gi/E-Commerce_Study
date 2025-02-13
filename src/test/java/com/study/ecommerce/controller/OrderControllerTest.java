63b5f0684c9203590acb037b4d4cb761320ab8f5
DANGHYEON <rlaalsrl3023@naver.com>
2025-02-13 20:24:13 +0900

    TEST : OrderControllerTest 주문조회

--- src/test/java/com/study/ecommerce/controller/OrderControllerTest.java
+++ src/test/java/com/study/ecommerce/controller/OrderControllerTest.java
@@ -1,17 +1,47 @@
 package com.study.ecommerce.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.ecommerce.config.CustomMockMember;
+import com.study.ecommerce.domain.*;
+import com.study.ecommerce.domain.type.OrderStatus;
+import com.study.ecommerce.domain.type.Payment;
+import com.study.ecommerce.repository.MemberRepository;
+import com.study.ecommerce.repository.OrderRepository;
+import com.study.ecommerce.repository.ProductCategoryRepository;
+import com.study.ecommerce.response.OrderResponse;
+import com.study.ecommerce.service.OrderService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https",uriHost = "api.ecommerce.com", uriPort = 433)
+@ExtendWith(RestDocumentationExtension.class)
 class OrderControllerTest {
 
     @Autowired
@@ -20,14 +50,90 @@ class OrderControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Test
-    void findAllOrders() {
+    @Autowired
+    private MemberRepository memberRepository;
 
+    @Autowired
+    private ProductCategoryRepository productCategoryRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @BeforeEach
+    public void clean(){
+        orderRepository.deleteAll();
+        productCategoryRepository.deleteAll();
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("주문 조회")
+    @CustomMockMember
+    void findAllOrders() throws Exception {
+        //given
+        Member member = Member.builder()
+                .email("Testing@naver.com")
+                .name("Test")
+                .password("ASDA515184424")
+                .role("ROLE_USER")
+                .build();
+
+        memberRepository.save(member);
+
+        ProductCategory productCategory = ProductCategory.builder()
+                .name("카테고리1")
+                .build();
+
+        productCategoryRepository.save(productCategory);
+
+        List<Product> requestProduct = IntStream.range(1,31).mapToObj(i ->
+                Product.builder()
+                        .name("물품" + i)
+                        .quantity(i)
+                        .price(i)
+                        .build()
+        ).toList();
+
+        requestProduct.forEach(product -> product.setCategory(productCategory));
+
+        productCategoryRepository.save(productCategory);
+
+
+
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken("Testing@naver.com", null, authorities)
+        );
+
+
+        Address address = Address.builder()
+                .city("서울")
+                .street("강남")
+                .zipcode("1111")
+                .build();
+
+        List<Order> requestOrders = IntStream.range(1,31).mapToObj(i ->
+                Order.builder()
+                        .orderStatus(OrderStatus.ORDER_COMPLETE)
+                        .address(address)
+                        .payment(Payment.CARD)
+                        .member(member)
+                        .totalPrice(i * 100)
+                        .build()
+        ).toList();
+
+        orderRepository.saveAll(requestOrders);
+
+
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/orders?page=1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
 
     }
 
     @Test
     void findOrderDate() {
+
     }
 
     @Test
